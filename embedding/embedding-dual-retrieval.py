5e18d45253f552b86ab758cafe208e5de8dfb03e
Karns, Allen B <allen.karns@vanderbilt.edu>
2024-02-09 14:06:23 -0600

    sep funcs to call and move client inside of each func for sep endpoint calls. add keyword summary and ts_query call

--- embedding/embedding-dual-retrieval.py
+++ embedding/embedding-dual-retrieval.py
@@ -1,11 +1,12 @@
 # set up retriever function that accepts a a query, user, and/or list of keys for where claus
-from openai import AzureOpenAI
+
 import os
 import json
 import psycopg2
 from pgvector.psycopg2 import register_vector
 from common.credentials import get_credentials, get_endpoint
 from common.validate import validated
+from shared_functions import generate_keywords, get_embeddings
 import logging
 
 ###Local Vars Remove Before Commit
@@ -28,46 +29,12 @@ pg_host = os.environ['RAG_POSTGRES_DB_READ_ENDPOINT']
 pg_user = os.environ['RAG_POSTGRES_DB_USERNAME']
 pg_database = os.environ['RAG_POSTGRES_DB_NAME']
 rag_pg_password = os.environ['RAG_POSTGRES_DB_SECRET']
-embedding_model_name = os.environ['EMBEDDING_MODEL_NAME']
-endpoints_arn = os.environ['ENDPOINTS_ARN']
-endpoint, api_key = get_endpoint(embedding_model_name, endpoints_arn)
 pg_password = get_credentials(rag_pg_password)
+keyword_model_name = os.environ['KEYWORD_MODEL_NAME']
 
 
-client = AzureOpenAI(
-    api_key = api_key,
-    azure_endpoint = endpoint,
-    api_version = "2023-05-15"
-)
 
-#db_connection = None
-#
-#def get_db_connection():
-#    global db_connection
-#    if db_connection is None or db_connection.closed:
-#        try:
-#            db_connection = psycopg2.connect(
-#                host=pg_host,
-#                database=pg_database,
-#                user=pg_user,
-#                password=pg_password,
-#                port=3306
-#            )
-#            logging.info("Database connection established.")
-#        except psycopg2.Error as e:
-#            logging.error(f"Failed to connect to the database: {e}")
-#            raise
-#    return db_connection
 
-def get_embeddings(text):
-    try:
-        print(f"Getting embeddings for: {text}")
-        response = client.embeddings.create(input=text, model=embedding_model_name)
-        return response.data[0].embedding
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        # Optionally, re-raise the exception if you want it to propagate
-        raise
 
 
 def get_top_similar_docs(query_embedding, current_user, src_ids=None, limit=5):
@@ -116,10 +83,10 @@ def get_top_similar_docs(query_embedding, current_user, src_ids=None, limit=5):
             cur.execute(sql_query, query_params)
             top_docs = cur.fetchall()
         
-    print(top_docs)
+    #print(top_docs)
     return top_docs
 
-def get_top_similar_ft_docs(user_input, current_user, src_ids=None, limit=5):
+def get_top_similar_ft_docs(input_keywords, current_user, src_ids=None, limit=5):
     with psycopg2.connect(
         host=pg_host,
         database=pg_database,
@@ -129,18 +96,11 @@ def get_top_similar_ft_docs(user_input, current_user, src_ids=None, limit=5):
     ) as conn:
         with conn.cursor() as cur:
             # Prepare SQL query and parameters based on whether src_ids are provided
-            query_params = [user_input, current_user]
-            src_clause = ""
             
-#            if src_ids:
-#                # Convert src_ids list to a format suitable for the IN clause in PostgreSQL
-#                src_ids_array = ",".join(map(str, src_ids))
-#                src_clause = f"AND src IN ({src_ids_array})"
-#            else:
-#                # If src_ids is not provided, we don't need an additional parameter for it
-#           
-#      src_clause = "" 
-            query_params.append(current_user)
+            query_params = [input_keywords, current_user]
+            src_clause = ""
+            query_params.append(input_keywords)
+            
             if src_ids:
                 # Convert src_ids list to a format suitable for the ANY clause in PostgreSQL
                 src_ids_array = "{" + ",".join(map(str, src_ids)) + "}"
@@ -149,10 +109,10 @@ def get_top_similar_ft_docs(user_input, current_user, src_ids=None, limit=5):
             # Append the limit to the query parameters
             query_params.append(limit)
             
-            print(f"Here is the query params {query_params}")
+            print(f"Here are the query params {query_params}")
             # Create SQL query string with a placeholder for the optional src_clause and a limit
             sql_query = f"""
-                SELECT content, src, locations, orig_indexes, char_index, owner_email, token_count, id
+                SELECT content, src, locations, orig_indexes, char_index, owner_email, token_count, id,
                     ts_rank_cd(to_tsvector('english', content), plainto_tsquery('english', %s)) AS text_rank
                 FROM embeddings 
                 WHERE owner_email = %s
@@ -165,6 +125,7 @@ def get_top_similar_ft_docs(user_input, current_user, src_ids=None, limit=5):
             # Execute the query with the correct number of parameters
             cur.execute(sql_query, query_params)
             top_ft_docs = cur.fetchall()
+            print(top_ft_docs)
         return top_ft_docs
 
 
@@ -179,11 +140,21 @@ def process_input_with_dual_retrieval(event, context, current_user, name, data):
 
     # Rest of your function ...
     embeddings = get_embeddings(user_input)
+    response = generate_keywords(user_input)
+    if response["statusCode"] == 200:
+        input_keywords = response["body"]["keywords"]
+        print(f"Keywords:\n{input_keywords}")
+    else:
+        # If there was an error, you can handle it accordingly.
+        error = response["body"]["error"]
+        print(f"Error occurred: {error}")    
+
     #print(f"This is some of my embeddings - {embeddings}")
+    
 
     # Step 1: Get documents related to the user input from the database
     related_docs = get_top_similar_docs(embeddings, current_user, src_ids, limit)
-    related_ft_docs = get_top_similar_ft_docs(user_input, current_user, src_ids, limit)
+    related_ft_docs = get_top_similar_ft_docs(input_keywords, current_user, src_ids, limit)
     related_docs.extend(related_ft_docs)
 
     # Return the related documents as a HTTP response
