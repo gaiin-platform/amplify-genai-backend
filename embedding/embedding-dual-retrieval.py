0e195c8e3a583dc2dd7f0928144cd5957c1d3a74
Karns, Allen B <allen.karns@vanderbilt.edu>
2024-02-29 21:35:43 -0600

    fix sql params to protect from injection

--- embedding/embedding-dual-retrieval.py
+++ embedding/embedding-dual-retrieval.py
@@ -9,6 +9,7 @@ from common.validate import validated
 from shared_functions import generate_keywords, generate_embeddings
 import logging
 
+
 pg_host = os.environ['RAG_POSTGRES_DB_READ_ENDPOINT']
 pg_user = os.environ['RAG_POSTGRES_DB_USERNAME']
 pg_database = os.environ['RAG_POSTGRES_DB_NAME']
@@ -19,7 +20,7 @@ api_version = os.environ['API_VERSION']
 
 pg_password = get_credentials(rag_pg_password)
 
-def get_top_similar_qas(query_embedding, src_ids=None, limit=5):
+def get_top_similar_qas(query_embedding, src_ids, limit=5):
     with psycopg2.connect(
         host=pg_host,
         database=pg_database,
@@ -40,35 +41,34 @@ def get_top_similar_qas(query_embedding, src_ids=None, limit=5):
             embedding_literal = "[" + ",".join(map(str, query_embedding)) + "]"
 
             # Prepare SQL query and parameters based on whether src_ids are provided
-            query_params = []
+            query_params = [embedding_literal, limit]  # query_embedding is already a list of floats
             src_clause = ""
 
             if src_ids:
                 # Convert src_ids list to a format suitable for the ANY clause in PostgreSQL
                 src_ids_array = "{" + ",".join(map(str, src_ids)) + "}"
-                
+            
                 
 
-            query_params.append(embedding_literal)
-            query_params.append(limit)  # Append the limit to the query parameters
+            query_params.insert(1, src_ids_array)  # Append the limit to the query parameters
+             # Append the limit to the query parameters
 
             # Create SQL query string with a placeholder for the optional src_clause and a limit
             sql_query = f"""
-                SELECT content, src, locations, orig_indexes, char_index, owner_email, token_count, id, ((qa_vector_embedding <#> '{embedding_literal}') * -1) AS distance
+                SELECT content, src, locations, orig_indexes, char_index, owner_email, token_count, id, ((qa_vector_embedding <#> %s::vector) * -1) AS distance
                 FROM embeddings 
-                WHERE owner_email = ((qa_vector_embedding <#> '{embedding_literal}') * -1) > .69
-                AND src = ANY('{src_ids_array}')
+                WHERE src = ANY(%s)  -- Use the ARRAY constructor for src_ids
                 ORDER BY distance DESC  -- Order by distance for ordering  
-                LIMIT {limit}  -- Use a placeholder for the limit
+                LIMIT %s  -- Use a placeholder for the limit
             """
-
-            cur.execute(sql_query)
+            #print(f"Here is the qa sql query {sql_query}")
+            cur.execute(sql_query, query_params)
             top_docs = cur.fetchall()
         
     return top_docs
 
 
-def get_top_similar_docs(query_embedding, src_ids=None, limit=5):
+def get_top_similar_docs(query_embedding, src_ids, limit=5):
     with psycopg2.connect(
         host=pg_host,
         database=pg_database,
@@ -80,44 +80,33 @@ def get_top_similar_docs(query_embedding, src_ids=None, limit=5):
         # Register pgvector extension
         register_vector(conn)
         with conn.cursor() as cur:
-
+            
             # Ensure the query_embedding is a list of floats
             assert isinstance(query_embedding, list), "Expected query_embedding to be a list of floats"
-            #print(f"here is the query embedding {query_embedding}")
-
-            # Convert the query_embedding list to a PostgreSQL array literal
-            embedding_literal = "[" + ",".join(map(str, query_embedding)) + "]"
-
-            # Prepare SQL query and parameters based on whether src_ids are provided
-            query_params = []
-            src_clause = ""
 
             if src_ids:
                 # Convert src_ids list to a format suitable for the ANY clause in PostgreSQL
                 src_ids_array = "{" + ",".join(map(str, src_ids)) + "}"
-                
-                
 
-            query_params.append(embedding_literal)
-            query_params.append(limit)  # Append the limit to the query parameters
+            # Prepare the query parameters
+            # Note: query_embedding is passed directly as a list of floats
+            query_params = [query_embedding, src_ids_array, limit]  # src_ids should be a tuple for psycopg2 to convert it to an array
 
-            # Create SQL query string with a placeholder for the optional src_clause and a limit
-            sql_query = f"""
-                SELECT content, src, locations, orig_indexes, char_index, owner_email, token_count, id, ((vector_embedding <#> '{embedding_literal}') * -1) AS distance
+            # Create SQL query string with placeholders for parameters
+            sql_query = """
+                SELECT content, src, locations, orig_indexes, char_index, owner_email, token_count, id, ((vector_embedding <#> %s::vector) * -1) AS distance
                 FROM embeddings 
-                WHERE ((vector_embedding <#> '{embedding_literal}') * -1) > .69
-                AND src = ANY('{src_ids_array}')
+                WHERE src = ANY(%s)  -- Use the ARRAY constructor for src_ids
                 ORDER BY distance DESC  -- Order by distance for ordering  
-                LIMIT {limit}  -- Use a placeholder for the limit
+                LIMIT %s  -- Use a placeholder for the limit
             """
-
-            cur.execute(sql_query)
+            #print(f"Here is the top match sql query {sql_query}")
+            cur.execute(sql_query, query_params)  # Pass the query parameters to the execute method
             top_docs = cur.fetchall()
         
-   
     return top_docs
 
-def get_top_similar_ft_docs(input_keywords, src_ids=None, limit=5):
+def get_top_similar_ft_docs(input_keywords, src_ids, limit=5):
     with psycopg2.connect(
         host=pg_host,
         database=pg_database,
@@ -127,10 +116,8 @@ def get_top_similar_ft_docs(input_keywords, src_ids=None, limit=5):
     ) as conn:
         with conn.cursor() as cur:
             # Prepare SQL query and parameters based on whether src_ids are provided
-            
-            query_params = [input_keywords]
-            
-            query_params.append(input_keywords)
+            src_ids_array = []
+            query_params = [input_keywords, input_keywords, input_keywords]
             
             if src_ids:
                 # Convert src_ids list to a format suitable for the ANY clause in PostgreSQL
@@ -144,18 +131,18 @@ def get_top_similar_ft_docs(input_keywords, src_ids=None, limit=5):
             # Create SQL query string with a placeholder for the optional src_clause and a limit
             sql_query = f"""
                 SELECT content, src, locations, orig_indexes, char_index, owner_email, token_count, id,
-                    ts_rank_cd(to_tsvector('english', content), to_tsquery('english', replace('{input_keywords}',' ','|'))) AS text_rank
+                    ts_rank_cd(to_tsvector('english', content), to_tsquery('english', replace(%s,' ','|'))) AS text_rank
                     
                 FROM embeddings 
-                WHERE ts_rank_cd(to_tsvector('english', content), to_tsquery('english', replace('{input_keywords}',' ','|'))) > .6
-                AND to_tsvector('english', content) @@ to_tsquery('english', replace('{input_keywords}',' ','|'))
-                AND src = ANY('{src_ids_array}')
+                WHERE ts_rank_cd(to_tsvector('english', content), to_tsquery('english', replace(%s,' ','|'))) > 0
+                AND to_tsvector('english', content) @@ to_tsquery('english', replace(%s,' ','|'))
+                AND src = ANY(%s)
                 ORDER BY text_rank DESC  -- Order by text rank for ordering
-                LIMIT {limit}  -- Use a placeholder for the limit
+                LIMIT %s  -- Use a placeholder for the limit
             """
-            
+            #print(f"Here is the ft sql query {sql_query}")
             # Execute the query with the correct number of parameters
-            cur.execute(sql_query)
+            cur.execute(sql_query, query_params)
             top_ft_docs = cur.fetchall()
         return top_ft_docs
     
@@ -214,12 +201,13 @@ def classify_src_ids_by_access(raw_src_ids, current_user):
 def process_input_with_dual_retrieval(event, context, current_user, name, data):
     data = data['data']
     content = data['userInput']
-    raw_src_ids = data['dataSources']
+    #raw_src_ids = data['dataSources']
+    src_ids = data['dataSources']
     limit = data['limit']
 
-    accessible_src_ids, access_denied_src_ids = classify_src_ids_by_access(raw_src_ids, current_user)
-    src_ids = accessible_src_ids
-    src_ids_message = f"Accessible src_ids: {accessible_src_ids}, Access denied src_ids: {access_denied_src_ids}"
+    #accessible_src_ids, access_denied_src_ids = classify_src_ids_by_access(raw_src_ids, current_user)
+    #src_ids = accessible_src_ids
+    #src_ids_message = f"Accessible src_ids: {accessible_src_ids}, Access denied src_ids: {access_denied_src_ids}"
 
     # Rest of your function ...
     embeddings = generate_embeddings(content)
@@ -234,12 +222,13 @@ def process_input_with_dual_retrieval(event, context, current_user, name, data):
 
 
     # Step 1: Get documents related to the user input from the database
-    related_docs = get_top_similar_docs(embeddings, current_user, src_ids, limit)
-    related_qas = get_top_similar_qas(embeddings, current_user, src_ids, limit)
+    related_docs = get_top_similar_docs(embeddings, src_ids, limit)
+    print(f"Here are the related docs {related_docs}")
+    related_qas = get_top_similar_qas(embeddings, src_ids, limit)
     related_docs.extend(related_qas)
-    related_ft_docs = get_top_similar_ft_docs(input_keywords, current_user, src_ids, limit)
+    related_ft_docs = get_top_similar_ft_docs(input_keywords, src_ids, limit)
     related_docs.extend(related_ft_docs)
-    related_docs.extend(src_ids_message)
+    #related_docs.extend(src_ids_message)
 
     # Return the related documents as a HTTP response
     return {"result":related_docs}
