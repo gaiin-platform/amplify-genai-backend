c131f6252c360875daba48a8dec7834ad331272d
Max Moundas <maxmoundas@Maxs-MacBook-Pro-2.local>
2024-04-17 14:56:28 -0500

    api gateway & bug fixes

--- data-disclosure/data_disclosure.py
+++ data-disclosure/data_disclosure.py
@@ -3,9 +3,17 @@ from boto3.dynamodb.conditions import Key
 import os
 import json
 from datetime import datetime
+import decimal
 
 s3 = boto3.client("s3")
 dynamodb = boto3.resource("dynamodb")
+
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, decimal.Decimal):
+            return str(obj)
+        return super(DecimalEncoder, self).default(obj)
 
 
 def generate_error_response(status_code, message):
@@ -29,6 +37,20 @@ def get_latest_version_number(table):
         return 0  # Return zero to indicate that there is no latest version
     latest_version = int(items[0]["version"])
     return latest_version
+
+
+# Helper function to get the latest version details
+def get_latest_version_details(table):
+    response = table.query(
+        KeyConditionExpression=Key("key").eq("latest"),
+        ScanIndexForward=False,  # Sorts the versions in descending order
+        Limit=1,
+    )
+    items = response.get("Items", [])
+    if not items:
+        return None  # Return None to indicate that there is no latest version
+    latest_version_details = items[0]
+    return latest_version_details
 
 
 # Function to upload a new data disclosure version
@@ -89,7 +111,7 @@ def upload_data_disclosure(event, context):
 
 # Check if a user's email has accepted the agreement in the DataDisclosureAcceptanceTable
 def check_data_disclosure_decision(event, context):
-    query_params = event.get("queryStringParameters", {})
+    query_params = event.get("queryStringParameters") or {}
     email = query_params.get("email")
     if not email:
         return generate_error_response(400, "Missing email parameter")
@@ -98,8 +120,13 @@ def check_data_disclosure_decision(event, context):
 
     try:
         response = table.get_item(Key={"user": email})
-        accepted = "Item" in response and response["Item"].get("accepted", False)
-        return {"statusCode": 200, "body": json.dumps({"accepted": accepted})}
+        acceptedDataDisclosure = "Item" in response and response["Item"].get(
+            "acceptedDataDisclosure", False
+        )
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"acceptedDataDisclosure": acceptedDataDisclosure}),
+        }
     except Exception as e:
         print(e)
         return generate_error_response(
@@ -115,17 +142,34 @@ def save_data_disclosure_decision(event, context):
         return {"statusCode": 400, "body": json.dumps({"error": "Invalid JSON format"})}
 
     email = body.get("email")
-    accepted = body.get("accepted")
+    accepted_data_disclosure = body.get("acceptedDataDisclosure")
 
-    if not isinstance(email, str) or not isinstance(accepted, bool):
+    if not isinstance(email, str) or accepted_data_disclosure not in (True, False):
         return generate_error_response(
             400, "Invalid input for saving data disclosure acceptance"
         )
 
-    table = dynamodb.Table(os.environ["DATA_DISCLOSURE_ACCEPTANCE_TABLE"])
+    versions_table = dynamodb.Table(os.environ["DATA_DISCLOSURE_VERSIONS_TABLE"])
+    acceptance_table = dynamodb.Table(os.environ["DATA_DISCLOSURE_ACCEPTANCE_TABLE"])
+
+    # Get the latest version details from the DataDisclosureVersionsTable
+    latest_version_details = get_latest_version_details(versions_table)
+    if not latest_version_details:
+        return generate_error_response(
+            500, "Error retrieving latest data disclosure version"
+        )
 
     try:
-        table.put_item(Item={"user": email, "accepted": accepted})
+        acceptance_table.put_item(
+            Item={
+                "user": email,
+                "acceptedDataDisclosure": accepted_data_disclosure,
+                "acceptedTimestamp": datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
+                "completedTraining": False,
+                "documentVersion": latest_version_details["version"],
+                "documentID": latest_version_details["id"],
+            }
+        )
         return {"statusCode": 200, "body": json.dumps({"message": "Record saved"})}
     except Exception as e:
         print(e)
@@ -150,7 +194,9 @@ def get_latest_data_disclosure(event, context):
         latest_agreement = items[0]
         return {
             "statusCode": 200,
-            "body": json.dumps({"latest_agreement": latest_agreement}),
+            "body": json.dumps(
+                {"latest_agreement": latest_agreement}, cls=DecimalEncoder
+            ),
         }
     except Exception as e:
         print(e)
