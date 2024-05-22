066e355bd705a69786138614c28b01990c5bd54f
Allen Karns <allen.karns@vanderbilt.edu>
2024-05-22 12:37:00 -0500

    update IAM Arn for Cognito Table and add logging

--- object-access/object_access.py
+++ object-access/object_access.py
@@ -156,6 +156,7 @@ def update_object_permissions(event, context, current_user, name, data, username
         principal_type = data.get('principalType')
         object_type = data.get('objectType')
 
+        # Get the DynamoDB table
         table = dynamodb.Table(table_name)
         
         for object_id in data_sources:
@@ -167,6 +168,7 @@ def update_object_permissions(event, context, current_user, name, data, username
             )
             items = query_response.get('Items')
 
+            # If there are no permissions, create the initial item with the current_user as the owner
             if not items:
                 print(" no permissions, create the initial item with the current_user as the owner")
                 table.put_item(Item={
@@ -174,10 +176,11 @@ def update_object_permissions(event, context, current_user, name, data, username
                     'principal_id': current_user,
                     'principal_type': principal_type,
                     'object_type': object_type,
-                    'permission_level': 'owner',
+                    'permission_level': 'owner',  # The current_user becomes the owner
                     'policy': policy
                 })
 
+            # Check if the current_user has 'owner' or 'write' permissions for the object_id
             owner_key = {
                 'object_id': object_id,
                 'principal_id': current_user
