10dc6d0d4820529a95998a77087ca8aec81981c2
Karely Rodriguez <karely.m.rodriguez.galvan@vanderbilt.edu>
2024-05-31 13:22:06 -0500

    Support for cloud conversations

--- amplify-lambda/common/permissions.py
+++ amplify-lambda/common/permissions.py
@@ -21,6 +21,9 @@ def can_read_share(user, data):
   # Read share automatically pulls data for the authenticated user
   return True
 
+def can_read(user, data):
+  return True
+
 def get_permission_checker(user, type, op, data):
   print("Checking permissions for user: {} and type: {} and op: {}".format(user, type, op))
   return permissions_by_state_type.get(type, {}).get(op, lambda user, data: False)
@@ -122,5 +125,23 @@ permissions_by_state_type = {
   },
   "/state/accounts/save": {
     "save": can_publish_item
+  },
+  "/state/conversation/upload": {
+    "conversation_upload": can_upload
+  },
+  "/state/conversation/get_multiple": {
+    "get_multiple_conversations": can_read
+  },
+  "/state/conversation/get": {
+    "read": can_read
+  },
+  "/state/conversation/get_all": {
+    "read": can_read
+  },
+  "/state/conversation/delete": {
+    "read": can_delete_item
+  },
+  "/state/conversation/delete_multiple": {
+    "delete_multiple_conversations": can_delete_item
   },
 }
