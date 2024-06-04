f867f3e5f71f7714eaa2ee7af7889fa80f7a51e4
Jules White <jules.white@gmail.com>
2024-06-04 06:56:29 -0500

    Removed obsolete datasource handler

--- amplify-lambda-js/datasource/external.js
+++ amplify-lambda-js/datasource/external.js
@@ -114,48 +114,6 @@ const createHandler = async (sourceType, config) => {
     }
 }
 
-const pdbsHandler = async (chatRequest, params, dataSource) => {
-    const dbId = dataSource.id.slice('pdbs://'.length);
-    const query = chatRequest.messages.slice(-1)[0].content;
-
-    const url = "https://dev-api.vanderbilt.ai/pdb/sql/llmquery"
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + params.account.accessToken
-        },
-        body: JSON.stringify({
-            data: {
-                "id":dbId,
-                "query":query,
-                "options": {
-                    "model": "gpt-4o"
-                }
-            }
-        })
-    });
-
-    const data = await response.json();
-
-    const contents = [{
-        "content": JSON.stringify(data.data),
-        "tokens": 1000,
-        "location": {
-            "key": dataSource.id
-        },
-        "canSplit": true
-    }];
-
-    const content =
-        {
-            "name": dataSource.id,
-            "content": contents
-        }
-
-    return content;
-}
-
 
 const getDatasourceHandler = async (sourceType, chatRequest, params, dataSource) => {
     const type = sourceType.split('://')[0];
