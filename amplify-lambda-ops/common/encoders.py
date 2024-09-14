6c839214836cd6fe3fad944f945fa3af22e114b9
Jules White <jules.white@gmail.com>
2024-09-14 10:00:48 -0500

    Fixed dependencies in amplify-lambda-ops

--- amplify-lambda-ops/common/encoders.py
+++ amplify-lambda-ops/common/encoders.py
@@ -1,6 +1,5 @@
 import json
 import decimal
-from pydantic import BaseModel, Field
 
 class DecimalEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -8,14 +7,7 @@ class DecimalEncoder(json.JSONEncoder):
             return int(obj)
         return super(DecimalEncoder, self).default(obj)
 
-def pydantic_encoder(obj):
-    if isinstance(obj, BaseModel):
-        return obj.dict()
-    raise TypeError(f"Object of type '{obj.__class__.__name__}' is not serializable")
-
 class CombinedEncoder(json.JSONEncoder):
     def default(self, obj):
-        if isinstance(obj, BaseModel):
-            return obj.model_dump()
         # Use the default DecimalEncoder for any other type it covers
         return DecimalEncoder.default(self, obj)
