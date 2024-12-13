ca0850462928ade1f9a25115d34f82d9e6eee736
Jules White <juleswhite@Juless-MacBook-Pro.local>
2024-12-13 12:48:27 -0600

    Working Google sheets get rows

--- amplify-lambda-assistants-api/integrations/google/service.py
+++ amplify-lambda-assistants-api/integrations/google/service.py
@@ -1,14 +1,27 @@
 from common.validate import validated
 from integrations.google.sheets import get_spreadsheet_rows
+from integrations.oauth import MissingCredentialsError
 
 
 @validated("get_rows")
 def get_sheet_rows(event, context, current_user, name, data):
     sheet_id = data['data']['sheetId']
     cell_range = data['data']['cellRange']
-    data = get_spreadsheet_rows(current_user, sheet_id, cell_range)
 
-    return {
-        "success": True,
-        "data": data
-    }
+    try:
+        data = get_spreadsheet_rows(current_user, sheet_id, cell_range)
+
+        return {
+            "success": True,
+            "data": data
+        }
+    except MissingCredentialsError as me:
+        return {
+            "success": False,
+            "error": str(me)
+        }
+    except Exception as e:
+        return {
+            "success": False,
+            "error": "Unknown server error."
+        }
