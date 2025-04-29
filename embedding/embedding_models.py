5bfd456f20e515785e5fee2539fd058abaff3529
Karely Rodriguez <karely.m.rodriguez.galvan@vanderbilt.edu>
2025-04-29 17:12:32 -0500

    Embeddings  - Support Default Models Changes

--- embedding/embedding_models.py
+++ embedding/embedding_models.py
@@ -5,26 +5,46 @@ dynamodb = boto3.resource('dynamodb')
 
 
 def get_embedding_models():
+    admin_table = dynamodb.Table(os.environ['AMPLIFY_ADMIN_DYNAMODB_TABLE'])
     model_rate_table = dynamodb.Table(os.environ["MODEL_RATE_TABLE"])
+    if (admin_table is None or model_rate_table is None):
+        return {"success": False, "message": "AMPLIFY_ADMIN_DYNAMODB_TABLE or MODEL_RATE_TABLE not set"}
+
+    embedding_model_id = None
+    qa_model_id = None
+    try:
+        print("Getting default model ids from admin table")
+        response = admin_table.get_item(Key={'config_id': 'defaultModels'})
+        if 'Item' in response:
+            default_models = response['Item']['data']
+            embedding_model_id = default_models['embeddings']
+            qa_model_id = default_models['qa']
+            print("embedding_model_id:", embedding_model_id)
+            print("qa_model_id:", qa_model_id)
+        else:
+            print(f"No Default Models Data Found")
+            return {"success": False, "message": "No Default Models Data Found"}
+    except Exception as e:
+        print(f"Error retrieving default models: {str(e)}")
+        return {"success": False, "message": "Error retrieving default models: {str(e)}"} 
+    
+    if embedding_model_id is None or qa_model_id is None:
+        return {"success": False, "message": "Could not find all default models"}
+    
+    
     defaults = {
         'embedding': None,
         'qa': None
     }
     try:
-        # Retrieve all items from the DynamoDB table
-        response = model_rate_table.scan()
-        items = response.get('Items', [])
-
-        # Check if there are more items (pagination)
-        while 'LastEvaluatedKey' in response:
-            response = model_rate_table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
-            items.extend(response.get('Items', []))
-        # Filter and find the default embedding and QA models
-        for item in items:
-            if item.get('DefaultEmbeddingsModel') is True:
-                defaults['embedding'] = {'model_id': item['ModelID'], 'provider': item['Provider']}
-            if item.get('DefaultQAModel') is True:
-                defaults['qa'] = {'model_id': item['ModelID'], 'provider': item['Provider']}
+        def get_provider(model_id, default_key):
+            response = model_rate_table.get_item(Key={'ModelID': model_id})
+            if 'Item' in response:
+                item = response['Item']
+                defaults[default_key] = {'model_id': item['ModelID'], 'provider': item['Provider']}
+        
+        get_provider(embedding_model_id, 'embedding')
+        get_provider(qa_model_id, 'qa')
 
     except Exception as e:
         print(f"Error retrieving default models: {str(e)}")
