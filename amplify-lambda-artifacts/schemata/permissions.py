2bdfd19498bb63b87da13f3530710d72f3dc2b9e
Karely Rodriguez <karely.m.rodriguez.galvan@vanderbilt.edu>
2025-07-02 13:21:21 -0700

    Refactor amplify-lambda-artifacts

--- amplify-lambda-artifacts/schemata/permissions.py
+++ amplify-lambda-artifacts/schemata/permissions.py
@@ -1,20 +1,28 @@
 def get_permission_checker(user, ptype, op, data):
-    print("Checking permissions for user: {} and type: {} and op: {}".format(user, ptype, op))
-    return permissions_by_state_type.get(ptype, {}).get(op, lambda for_user, with_data: False)
+    print(
+        "Checking permissions for user: {} and type: {} and op: {}".format(
+            user, ptype, op
+        )
+    )
+    return permissions_by_state_type.get(ptype, {}).get(
+        op, lambda for_user, with_data: False
+    )
 
 
 def can_read(user, data):
     return True
 
+
 def can_save(user, data):
     return True
+
 
 def can_share(user, data):
     return True
 
+
 def can_delete(user, data):
     return True
-
 
 
 """
@@ -26,19 +34,9 @@ is a function that takes a user and data and returns if the
 user can do the operation.
 """
 permissions_by_state_type = {
-    "/artifacts/get_all": {
-        "read": can_read
-    },
-    "/artifacts/get": {
-        "read": can_read
-    },
-    "/artifacts/delete": {
-        "delete": can_delete
-    },
-    "/artifacts/save": {
-        "save": can_save
-    },
-    "/artifacts/share": {
-        "share": can_share
-    },
+    "/artifacts/get_all": {"read": can_read},
+    "/artifacts/get": {"read": can_read},
+    "/artifacts/delete": {"delete": can_delete},
+    "/artifacts/save": {"save": can_save},
+    "/artifacts/share": {"share": can_share},
 }
