31d691991f2401962992ed5930a67e770b50c99a
Karely Rodriguez <karely.m.rodriguez.galvan@vanderbilt.edu>
2025-08-18 13:17:11 -0500

    Enhancements - Integrate thinking message support
    - Added `getBudgetTokens` function to calculate budget tokens based on reasoning effort levels.
    - Updated `chatBedrock` and `chatWithDataStateless` functions to include reasoning configuration and budget tokens for Bedrock and Gemini models.
    - Refactored API key retrieval in `secrets.js` for better flexibility.
    - Enhanced `bedrockConverseTransform` to handle reasoning content and send status events to the stream.

--- amplify-lambda-js/common/params.js
+++ amplify-lambda-js/common/params.js
@@ -1,7 +1,7 @@
 //Copyright (c) 2024 Vanderbilt University  
 //Authors: Jules White, Allen Karns, Karely Rodriguez, Max Moundas
 
-import {chat} from "../azure/openai.js";
+import {chat as openaiChat} from "../azure/openai.js";
 import {chat as geminiChat} from "../gemini/gemini.js";
 import { chatBedrock } from "../bedrock/bedrock.js";
 import {getLLMConfig} from "../common/secrets.js";
@@ -54,10 +54,28 @@ export const getMaxTokens = (params) => {
     return params.options.maxTokens;
 }
 
+export const getBudgetTokens = (params, maxTokens) => {
+    const reasoning_effort = params.options.reasoningLevel ?? "low";
+    let budget_tokens = 1024;
+    switch (reasoning_effort) {
+        case "medium":
+            budget_tokens = 2048;
+            break;
+        case "high":
+            budget_tokens = 4096;
+            break;
+    }
+    if (budget_tokens >= maxTokens) {
+        budget_tokens = maxTokens / 2;
+    }
+    return budget_tokens;
+
+}
+
 export const getChatFn = (model, body, writable, context) => {
 
     if (isOpenAIModel(model.id)) {
-        return chat(getLLMConfig, body, writable, context);
+        return openaiChat(getLLMConfig, body, writable, context);
     } else if (model.provider === 'Bedrock') {
         return chatBedrock(body, writable, context);
     } else if (isGeminiModel(model.id)) {
