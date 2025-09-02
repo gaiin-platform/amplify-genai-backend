6fd0c668e9fc204f3cc1625cc913668d311a1fd1
Karely Rodriguez <karely.m.rodriguez.galvan@vanderbilt.edu>
2025-09-02 16:24:49 -0500

    Add user management functions to serverless configuration
    - Introduced two new Lambda functions: `create_or_update_user` and `upgrade_user` for handling user management.
    - Updated `serverless.yml` to include HTTP event triggers for both functions, enabling POST requests at `/user/create` and `/user/upgrade`.
    - Created a new `users.py` file containing the implementation for these functions, which currently logs the event and context for further development.

---
+++ object-access/users.py
@@ -0,0 +1,14 @@
+
+def create_or_update_user(event, context):
+    print("Creating or updating user")
+    print(event)
+    print(context)
+    pass
+
+
+
+def upgrade_user(event, context):
+    print("Upgrading user")
+    print(event)
+    print(context)
+    pass
