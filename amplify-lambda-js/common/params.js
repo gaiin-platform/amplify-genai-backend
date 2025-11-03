103db1337a7eed766bb74c4f37ddb303dd8aa541
Jason Bradley <jasonbrd@amazon.com>
2025-11-03 07:01:17 -0600

    Correct logic for getChatFn based on provider

--- amplify-lambda-js/common/params.js
+++ amplify-lambda-js/common/params.js
@@ -73,19 +73,15 @@ export const getBudgetTokens = (params, maxTokens) => {
 }
 
 export const getChatFn = (model, body, writable, context) => {
-
-    if (isOpenAIModel(model.id)) {
-        return openaiChat(getLLMConfig, body, writable, context);
-    } else if (model.provider === 'Bedrock') {
-        return chatBedrock(body, writable, context);
-    } else if (isGeminiModel(model.id)) {
-        return geminiChat(body, writable, context);
-    } else {
-        console.log(`Error: Model ${model} does not have a corresponding chatFn`)
-        return null;
-    }
+    const chatFunctions = {
+        'Bedrock': () => chatBedrock(body, writable, context),
+        'OpenAI': () => openaiChat(getLLMConfig, body, writable, context),
+        'Gemini': () => geminiChat(body, writable, context)
+    };
+    
+    return chatFunctions[model.provider]?.() ?? 
+        (console.log(`Error: Model ${model} does not have a corresponding chatFn`), null);
 }
-
 
 export const isOpenAIModel = (modelId) => {
     return modelId && (modelId.includes("gpt") || /^o\d/.test(modelId));
