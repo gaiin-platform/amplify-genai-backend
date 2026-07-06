da0f2cb4ea8ea5855b34cd0891102dbb10025ac1
Karely Rodriguez <karely.m.rodriguez.galvan@vanderbilt.edu>
2026-07-06 17:41:01 -0500

    fix: update environment variable for S3 bucket name in shared inbox integration

--- amplify-lambda-assistants-api-office365/integrations/o365/shared_inbox.py
+++ amplify-lambda-assistants-api-office365/integrations/o365/shared_inbox.py
@@ -326,7 +326,7 @@ def download_shared_mailbox_attachment(
                 if not content_response.ok:
                     _handle_graph_error(content_response)
 
-                bucket_name = os.environ["SHARED_MAILBOX_ATTACHMENTS_BUCKET"]
+                bucket_name = os.environ["S3_CONSOLIDATION_BUCKET_NAME"]
                 safe_filename = attachment_metadata.get("name", "attachment")
                 s3_key = f"sharedMailboxAttachments/{current_user}/{uuid.uuid4().hex}/{safe_filename}"
 
